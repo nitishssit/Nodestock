2a0050aa8572d0575bdea53407db227c04307bc5
Niteesh <nitishssit@gmail.com>
2023-04-19 10:44:11 +0530

    initial commit

---
+++ stock.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const {engine} = require('express-handlebars');
+const path= require('path');
+
+const PORT = process.env.PORT || 5000;
+
+//Set handlebars middleware
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+
+const otherstuff = "Hello there, this is different stuff!";
+//Set handlebar routes
+app.get('/', function(req,res){
+	res.render('home',{
+		stuff : otherstuff
+	});
+});
+
+//Set static folder 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => console.log('Server listening on port ' + PORT));
+
